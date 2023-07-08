d3cbd38421e6aca383a79698ef94fa1e0a089302
ram kumar <rammaniyari@gmail.com>
2023-07-08 21:28:28 +0530

    getTodById router is created

--- TodoApp/controllers/getTodo.js
+++ TodoApp/controllers/getTodo.js
@@ -22,3 +22,34 @@ exports.getTodo = async (req, res) => {
     }
 
 }
+
+
+
+exports.getTodoById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const todo = await Todo.findById({ _id: id });
+
+        if (!todo) {
+            return res.status(404).json({
+                success: false,
+                message: "No Data found with this id "
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: todo,
+            message: "Todo data successfully fetched "
+        })
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            data: "Internal server error",
+            message: error.message
+        })
+    }
+}
--- TodoApp/routers/todoRouter.js
+++ TodoApp/routers/todoRouter.js
@@ -5,10 +5,11 @@ const router = express.Router();
 
 const { createTodo } = require("../controllers/createTodo");
 
-const {getTodo} = require("../controllers/getTodo")
+const {getTodo, getTodoById} = require("../controllers/getTodo")
 
 router.post("/createTodo", createTodo);
 router.get("/getTodos",getTodo);
+router.get("/getTodo/:id",getTodoById);
 
 
 module.exports = router;
