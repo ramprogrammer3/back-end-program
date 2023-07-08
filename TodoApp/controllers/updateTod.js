84b1f08aa418ff5e84729a733d1272a9b1a2f29b
ram kumar <rammaniyari@gmail.com>
2023-07-08 21:41:41 +0530

    updateTodo router is creatd successfully

---
+++ TodoApp/controllers/updateTod.js
@@ -0,0 +1,33 @@
+
+const Todo = require("../models/Todo");
+
+exports.updateTodo = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+        const { title, description } = req.body;
+
+        const todo = await Todo.findByIdAndUpdate(
+
+            { _id: id },
+            { title, description, updatedAt: Date.now() }
+        )
+
+        res.status(200).json({
+            success: true,
+            data: todo,
+            message: "Updated successfully"
+        })
+
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            data: "Internal server error",
+            message: error.message
+        })
+
+    }
+}
--- TodoApp/routers/todoRouter.js
+++ TodoApp/routers/todoRouter.js
@@ -7,9 +7,13 @@ const { createTodo } = require("../controllers/createTodo");
 
 const {getTodo, getTodoById} = require("../controllers/getTodo")
 
+const {updateTodo} = require("../controllers/updateTod")
+
 router.post("/createTodo", createTodo);
 router.get("/getTodos",getTodo);
 router.get("/getTodo/:id",getTodoById);
+router.put("/updateTodo/:id",updateTodo);
+
 
 
 module.exports = router;
