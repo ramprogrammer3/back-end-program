3f4c720cb3b97e93eb03b7578246eb7b81eca8ee
ram kumar <rammaniyari@gmail.com>
2023-07-08 18:42:18 +0530

    todo app basic setup

---
+++ TodoApp/index.js
@@ -0,0 +1,13 @@
+
+const express = require("express")
+const app = express();
+
+require("dotenv").config();
+
+const port = process.env.PORT || 8080;
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
