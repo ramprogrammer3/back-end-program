aeb24bf89264b4f23c26d4aa5be97960db6fda92
ram kumar <rammaniyari@gmail.com>
2023-07-08 21:17:42 +0530

    todo getTodos router is created

--- TodoApp/routers/todoRouter.js
+++ TodoApp/routers/todoRouter.js
@@ -5,7 +5,10 @@ const router = express.Router();
 
 const { createTodo } = require("../controllers/createTodo");
 
+const {getTodo} = require("../controllers/getTodo")
+
 router.post("/createTodo", createTodo);
+router.get("/getTodos",getTodo);
 
 
 module.exports = router;
