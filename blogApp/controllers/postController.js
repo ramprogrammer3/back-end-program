6581959040e92227367b26176cacaf1cb9fee13f
ram kumar <rammaniyari@gmail.com>
2023-07-15 21:05:31 +0530

    adding create post controller and router

--- blogApp/controllers/postController.js
+++ blogApp/controllers/postController.js
@@ -1,1 +1,27 @@
 
+const Post = require("../models/postModel");
+
+exports.createPost = async (req, res) => {
+
+    try {
+
+        const { title, body } = req.body;
+
+        const post = new Post({
+            title,
+            body
+        });
+
+        const savedPost = await post.save();
+
+        res.json({
+            post: savedPost,
+        })
+
+    } catch (error) {
+
+        return res.status(400).json({
+            error: "Error while creating Post",
+        })
+    }
+}
--- blogApp/routers/blog.js
+++ blogApp/routers/blog.js
@@ -1,5 +1,13 @@
 const express = require("express")
 const router = express.Router();
 
+// import controler
+const { createComment } = require("../controllers/commentController");
+const { createPost } = require("../controllers/postController");
+
+
+router.post("/comments/create", createComment);
+router.post("/posts/create", createPost)
+
 
 module.exports = router;
