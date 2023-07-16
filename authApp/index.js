b545f56adbcde12805058c7424d2e893b46a7643
ram kumar <rammaniyari@gmail.com>
2023-07-16 22:24:45 +0530

    creating router and activate server

--- authApp/index.js
+++ authApp/index.js
@@ -5,7 +5,20 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 4000;
 
-
+//middlerware to json parse
 app.use(express.json());
+
+require("./config/database").connect();
+
+// router import and mount
+
+const user = require("./routers/userRoutes");
+app.use("/api/v1", user);
+
+// activate server
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
 
 
