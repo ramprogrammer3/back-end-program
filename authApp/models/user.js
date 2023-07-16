40779da51afeb9fa6d5bd5f2b4ddff507a6d2a2b
ram kumar <rammaniyari@gmail.com>
2023-07-16 22:31:29 +0530

    adding schema for user

---
+++ authApp/models/user.js
@@ -0,0 +1,33 @@
+
+const mongoose = require("mongoose")
+
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    roll: {
+        type: String,
+        required: true,
+        enum: ["Admin", "Student", "Visitor"]
+    },
+
+})
+
+
+module.exports = mongoose.model("user", userSchema);
--- authApp/routers/userRoutes.js
+++ authApp/routers/userRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const router = express.Router();
 
+const { login, signup } = require("../controllers/Auth");
+
+router.post("/login", login);
+router.post("/signup", signup);
 
 
 
