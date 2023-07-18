8e78dc65e133c350c8f86b50c6eeb562aca8cd46
ram kumar <rammaniyari@gmail.com>
2023-07-18 09:08:01 +0530

    adding middlerware for protected route

--- authApp/routers/userRoutes.js
+++ authApp/routers/userRoutes.js
@@ -3,8 +3,33 @@ const router = express.Router();
 
 const { signup, login } = require("../controllers/Auth");
 
+const { auth, isStudent, isAdmin } = require("../middlewares/auth");
+
 router.post("/login", login);
 router.post("/signup", signup);
+
+// protected route
+
+router.get("/test", auth, (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to the procted route for TESt"
+    })
+})
+
+router.get("/student", auth, isStudent, (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to the procted route for Students"
+    })
+})
+
+router.get("/admin", auth, isAdmin, (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to the procted route for Admin"
+    })
+})
 
 
 
